34c74b24b79cafd46d23d3d5a11ffdd4c1bb1dcc
jung_developer <59546994+wjdgml3092@users.noreply.github.com>
2023-06-28 16:58:25 +0900

    Updates

---
+++ 252f366e-fa33d69f5fca51b4a7c3.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[228],{5434:function(t,a,e){e.d(a,{F8X:function(){return n}});var r=e(4405);function n(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0V0z"}},{tag:"path",attr:{d:"M22 6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6zm-2 0l-8 5-8-5h16zm0 12H4V8l8 5 8-5v10z"}}]})(t)}}}]);
+//# sourceMappingURL=252f366e-fa33d69f5fca51b4a7c3.js.map
