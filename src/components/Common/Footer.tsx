054834f6d0d8e3e9faa3f2a6dfff9526dd151d5e
jung_developer <59546994+wjdgml3092@users.noreply.github.com>
2023-06-30 15:28:50 +0900

    feat: footer position fix

--- src/components/Common/Footer.tsx
+++ src/components/Common/Footer.tsx
@@ -3,11 +3,13 @@ import styled from '@emotion/styled'
 const FooterWrapper = styled.footer`
   display: grid;
   place-items: center;
-  margin-top: auto;
-  padding: 50px 0;
+  padding: 20px 0;
   font-size: 15px;
   text-align: center;
   line-height: 1.5;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
 
   @media (max-width: 768px) {
     font-size: 13px;
