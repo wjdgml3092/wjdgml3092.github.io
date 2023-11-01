ccfa5f0453319efe0900ff234539bc48a01f65ac
jung_developer <59546994+wjdgml3092@users.noreply.github.com>
2023-11-01 20:37:34 +0900

    Updates

---
+++ Colors.ts
@@ -0,0 +1,13 @@
+const colors = {
+  red: '#D22C26',
+  green: '#82AB42',
+  white: '#FFFFFF',
+  tan: '#D8D3C4',
+  beige: '#F4ECCF',
+  dimgray: '#7D756D',
+  softgreen: '#E4EEC0',
+  softblue: '#D6E4F2',
+  text1: 'red',
+}
+
+export default colors
